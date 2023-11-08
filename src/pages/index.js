130c588807edca5772fb678d098b79571368ddf1
vedant-3010 <vedantdandgepro@gmail.com>
2023-11-08 11:09:15 +0530

    toolbox added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Menu from "@/components/Menu";
+import ToolBox from "@/components/Toolbox";
 
 
 export default function Home() {
   return (
     <>
-    <Menu/>
+      <Menu />
+      <ToolBox />
     </>
   )
 }
