62b77c39dbb4b7a7f3b90e112740658a41a3da0a
vedant-3010 <vedantdandgepro@gmail.com>
2024-01-27 03:43:50 +0530

    added deployed backend url

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const URL = 'http://localhost:5000'
+const URL = process.env.NODE_ENV === 'production' ? 'https://kanvasdraw-server.onrender.com' : 'http://localhost:5000'
 export const socket = io(URL);
