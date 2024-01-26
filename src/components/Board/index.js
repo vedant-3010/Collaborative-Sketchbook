87f768e95f9dccc30aaed57b1386ed9a7ea8bb21
vedant-3010 <vedantdandgepro@gmail.com>
2024-01-27 03:04:10 +0530

    added feature to download and undo/redo on canvas

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -10,8 +10,29 @@ const Board = () => {
     const drawHistory = useRef([])
     const historyPointer = useRef(0)
     const shouldDraw = useRef(false)
-    const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu)
-    const { color, size } = useSelector((state) => state.toolbox[activeMenuItem])
+    const {activeMenuItem, actionMenuItem} = useSelector((state) => state.menu)
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
+
+
+    useEffect(() => {
+        if (!canvasRef.current) return
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d')
+
+        if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
+            const URL = canvas.toDataURL()
+            const anchor = document.createElement('a')
+            anchor.href = URL
+            anchor.download = 'sketch.jpg'
+            anchor.click()
+        } else  if (actionMenuItem === MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO) {
+            if(historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO) historyPointer.current -= 1
+            if(historyPointer.current < drawHistory.current.length - 1 && actionMenuItem === MENU_ITEMS.REDO) historyPointer.current += 1
+            const imageData = drawHistory.current[historyPointer.current]
+            context.putImageData(imageData, 0, 0)
+        }
+        dispatch(actionItemClick(null))
+    }, [actionMenuItem, dispatch])
 
     useEffect(() => {
         if (!canvasRef.current) return
